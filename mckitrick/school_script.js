b0e5b2581a25b5135f3abff93fcb695dc5f9489a
Mark Strendin <mark.strendin@lskysd.ca>
2016-09-15 09:49:30 -0600

    Initial commit

---
+++ mckitrick/school_script.js
@@ -0,0 +1,18 @@
+/* ******************************************** */
+/* * Editable values                          * */
+/* ******************************************** */
+
+// School name, as you would like it displayed
+var school_name = "McKitrick Community School";
+
+// Max connection speeds (in megabits per second)
+var school_maxmbps_in = 10;
+var school_maxmbps_out = 10;
+
+// Is the SNMP data reversed? Is the "in" field actually the "out" traffic?
+var school_snmp_is_reversed = false;
+
+// URLs to the JSON files from the SNMP monitor. These should NOT start with "http://dashboard.lskysd.ca"
+// so use relative links only.
+var school_snmp_json_path = "/StrendinMonitor/JSON/bySNMPThroughputSensor.aspx?sensorid=24";
+var school_ping_json_path = "/StrendinMonitor/JSON/byPingLatencySensor.aspx?sensorid=22";
